18b5f334e9015f4dce35b5a8e6adff145dc14760
Guru <gprasad22@hotmail.com>
2019-05-01 10:23:12 +0100

    initial checkin

---
+++ PivotSecurity/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PivotSecurity
+{
+    class Account
+    {
+        private static string test_public_key = "";
+        private static string test_private_key = "";
+        private static string prod_public_key = "";
+        private static string prod_private_key = "";
+
+        var client = new RestClient("http://example.com");
+        var request = new RestRequest("resource/{id}");
+        request.AddHeader("header", "value");
+        request.AddJsonObject(@object);
+            var response = client.Post(request);
+        var content = response.Content;
+    }
+}
