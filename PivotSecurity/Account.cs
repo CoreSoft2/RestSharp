2089c8c4ce01990876dac4ccb1c4e787d839cd6e
Guru <gprasad22@hotmail.com>
2019-05-01 18:47:22 +0100

    initial checkin

--- PivotSecurity/Account.cs
+++ PivotSecurity/Account.cs
@@ -1,21 +1,143 @@
-﻿using System;
+﻿using PivotSecurity.Authenticators;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace PivotSecurity
 {
     class Account
     {
-        private static string test_public_key = "";
-        private static string test_private_key = "";
-        private static string prod_public_key = "";
-        private static string prod_private_key = "";
+        private static string public_key = "";
+        private static string private_key = "";
 
-        var client = new RestClient("http://example.com");
-        var request = new RestRequest("resource/{id}");
-        request.AddHeader("header", "value");
-        request.AddJsonObject(@object);
+        public Account(string _public_key, string _private_key)
+        {
+            Account.public_key = _public_key;
+            Account.private_key = _private_key;
+        }
+
+        public string CreateCustomer(string uid, string email, string channel)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/info");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\", \"channel\":\"" + channel + "\"}");
             var response = client.Post(request);
-        var content = response.Content;
+            return response.Content;
+        }
+        public CustomerInfo Info(string uid, string email)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/info");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\""+uid+"\", \"email\":\""+email+"\"}");
+            var response = client.Post(request);
+            CustomerInfo customerInfo = JsonConvert.DeserializeObject<CustomerInfo>(response.Content);
+            return customerInfo;
+        }
+        public string RiskScore(string uid, string email)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/riskscore");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\"}");
+            var response = client.Post(request);
+            return response.Content;
+        }
+
+        public string QRCode(string uid, string email)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/qrcode");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\"}");
+            var response = client.Post(request);
+            return response.Content;
+        }
+        public string AuthCode(string uid, string email)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/info");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\"}");
+            var response = client.Post(request);
+            return response.Content;
+        }
+        public List<AccessLog> AccessLogs(string uid, string email)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/logs");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\"}");
+            var response = client.Post(request);
+            List<AccessLog> accessLogs = JsonConvert.DeserializeObject<List<AccessLog>>(response.Content);
+            return accessLogs;
+        }
+        public string Lock(string uid, string email)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/lock");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\"}");
+            var response = client.Post(request);
+            return response.Content;
+        }
+        public string Unlock(string uid, string email)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/unlock");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\"}");
+            var response = client.Post(request);
+            return response.Content;
+        }
+        public string TrainMachineLearning(string uid, string email, string csvdata)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/trainml");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\", \"data\":\""+csvdata+"\"}");
+            var response = client.Post(request);
+            return response.Content;
+        }
+        public string TestMachineLearning(string uid, string email, string csvdata)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/info");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\", \"data\":\"" + csvdata + "\"}");
+            var response = client.Post(request);
+            return response.Content;
+        }
+        public string GetAuthWithMetaData(string uid, string email, string metadata)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/authwithmetadata");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\", \"metadata\":\"" + metadata + "\"}");
+            var response = client.Post(request);
+            return response.Content;
+        }
+        public string SendAuthCodeWithMetadata(string uid, string email, string metadata)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/sendauthwithmetadata");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\", \"metadata\":\"" + metadata + "\"}");
+            var response = client.Post(request);
+            return response.Content;
+        }
+        public string VerifyAuthWithMetadata(string uid, string email, string code)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("account/verifywithmetadata");
+            client.Authenticator = new HttpBasicAuthenticator(private_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\", \"code\":\"" + code + "\"}");
+            var response = client.Post(request);
+            return response.Content;
+
+        }
+
     }
 }
--- PivotSecurity/Customer.cs
+++ PivotSecurity/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PivotSecurity.Authenticators;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,31 @@ namespace PivotSecurity
 {
     class Customer
     {
+        private static string public_key = "";
+        private static string private_key = "";
+        public Customer(string _public_key, string _private_key)
+        {
+            Customer.public_key = _public_key;
+            Customer.private_key = _private_key;
+        }
+
+        public string AuthenticateCustomer(string uid, string email, string code)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("customer/auth");
+            client.Authenticator = new HttpBasicAuthenticator(public_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\"}");
+            var response = client.Post(request);
+            return response.Content;
+        }
+        public string VerifyCustomer(string uid, string email, string code)
+        {
+            var client = new RestClient("https://api.povotsecurity.com/api/");
+            var request = new RestRequest("customer/verify");
+            client.Authenticator = new HttpBasicAuthenticator(public_key, "");
+            request.AddJsonBody("{\"uid\":\"" + uid + "\", \"email\":\"" + email + "\", \"code\":\"" + code + "\"}");
+            var response = client.Post(request);
+            return response.Content;
+        }
     }
 }
